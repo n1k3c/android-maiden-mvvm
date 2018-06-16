b7eca978c9c724eb48be8150a67df4765e3df24a
Nikola Curilovic <ncurilovic@gmail.com>
2018-06-16 20:21:19 +0200

    Add tests

--- app/src/test/java/n1x0nj4/maidenmvvm/ui/restaurants/BaseViewModelTest.kt
+++ app/src/test/java/n1x0nj4/maidenmvvm/ui/restaurants/BaseViewModelTest.kt
@@ -1,8 +1,10 @@
 package n1x0nj4.maidenmvvm.ui.restaurants
 
 import agency.andromeda.planetreads.util.RxSchedulersOverrideRule
+import android.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Before
 import org.junit.Rule
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -12,6 +14,9 @@ abstract class BaseViewModelTest() {
     @Rule
     @JvmField
     var testSchedulerRule = RxSchedulersOverrideRule()
+
+    @get: Rule
+    var rule: TestRule = InstantTaskExecutorRule()
 
     @Before
     abstract fun setUp()
--- app/src/test/java/n1x0nj4/maidenmvvm/ui/restaurants/RestaurantViewModelTest.kt
+++ app/src/test/java/n1x0nj4/maidenmvvm/ui/restaurants/RestaurantViewModelTest.kt
@@ -1,11 +1,56 @@
 package n1x0nj4.maidenmvvm.ui.restaurants
 
-import org.junit.Assert.*
+import android.arch.lifecycle.Observer
+import io.reactivex.Single
+import n1x0nj4.maidenmvvm.model.Restaurant
+import n1x0nj4.maidenmvvm.repository.RestaurantsRepository
+import n1x0nj4.maidenmvvm.util.Status
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when` as whenever
 
 class RestaurantViewModelTest : BaseViewModelTest() {
 
-    override fun setUp() {
+    private lateinit var restaurantViewModel: RestaurantViewModel
 
+    @Mock
+    private
+    lateinit var restaurantRepository: RestaurantsRepository
+
+    @Mock
+    private
+    lateinit var restaurantList: List<Restaurant>
+
+    override fun setUp() {
+        restaurantViewModel = RestaurantViewModel(restaurantRepository)
     }
 
+    @Test
+    fun shouldReturnListOfRestaurants() {
+        whenever(restaurantRepository.fetchRestaurantsFromAPI()).thenReturn(Single.just(restaurantList))
+
+        val result = mock(Observer::class.java)
+
+        restaurantViewModel.restaurantResult.observeForever(result as Observer<List<Restaurant>>)
+
+        restaurantViewModel.getRestaurants()
+
+        assertEquals(restaurantViewModel._status.value, Status.SUCCESS)
+
+        verify(restaurantRepository).fetchRestaurantsFromAPI()
+    }
+
+    @Test
+    fun shouldReturnErrorWhileLoadingRestaurants() {
+        whenever(restaurantRepository.fetchRestaurantsFromAPI()).thenReturn(Single.error(Throwable()))
+
+        restaurantViewModel.getRestaurants()
+
+        assertEquals(restaurantViewModel._status.value, Status.ERROR)
+
+        verify(restaurantRepository).fetchRestaurantsFromAPI()
+    }
 }
