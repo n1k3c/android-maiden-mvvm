28435d747cf74434842dfdae8c57c970de4476a6
Nikola Curilovic <ncurilovic@gmail.com>
2018-07-06 17:10:00 +0200

    Code reformat

--- data/src/main/java/com/n1x0nj4/data/cache/mapper/CachedRestaurantMapper.kt
+++ data/src/main/java/com/n1x0nj4/data/cache/mapper/CachedRestaurantMapper.kt
@@ -1,16 +1,16 @@
 package com.n1x0nj4.data.cache.mapper
 
-import com.n1x0nj4.data.cache.model.CachedProject
+import com.n1x0nj4.data.cache.model.CachedRestaurant
 import com.n1x0nj4.data.remote.model.RestaurantResponse
 import javax.inject.Inject
 
-class CachedProjectMapper @Inject constructor() : CacheMapper<CachedProject, RestaurantResponse> {
+class CachedRestaurantMapper @Inject constructor() : CacheMapper<CachedRestaurant, RestaurantResponse> {
 
-    override fun mapFromCached(type: CachedProject): RestaurantResponse {
+    override fun mapFromCached(type: CachedRestaurant): RestaurantResponse {
         return RestaurantResponse(type.name, type.address, type.latitude, type.latitude)
     }
 
-    override fun mapToCached(type: RestaurantResponse): CachedProject {
-        return CachedProject(type.name, type.address, type.latitude, type.latitude)
+    override fun mapToCached(type: RestaurantResponse): CachedRestaurant {
+        return CachedRestaurant(type.name, type.address, type.latitude, type.latitude)
     }
 }
